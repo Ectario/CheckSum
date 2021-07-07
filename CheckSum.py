6cd031b5729223ae24c3b0ff70359a3226c69166
DCarlier <61197119+Ectario@users.noreply.github.com>
2021-07-07 15:52:59 +0200

    Add files via upload

---
+++ CheckSum.py
@@ -0,0 +1,67 @@
+from enum import Enum, auto
+import hashlib
+import sys
+
+def usage():
+    print("Usage : FileChecker.py [path] [algorithm] compare=[Hash to compare]\n \
+            Or : FileChecker.py [path] [algorithn]", file=sys.stderr)
+
+#Managed hash algorithm for this version
+class Hash(Enum):
+    SHA256 = auto()
+    SHA1 = auto()
+    MD5 = auto()
+
+
+def getHash(file, algo):
+    hash = None
+    # Check if the wanted algo is managed
+    for i in list(Hash): 
+        if i.name == algo:
+            hash = i
+            break
+    if not hash:
+        print("Wrong hash algorithm", file=sys.stderr)
+        usage()
+    
+    # Switch case the hash
+    if hash.name == Hash.SHA256.name:
+        return hashlib.sha256(file)
+    elif hash.name == Hash.SHA1.name:
+        return hashlib.sha1(file)
+    elif hash.name == Hash.MD5.name:
+        return hashlib.md5(file)
+    
+    
+if __name__ == '__main__':
+    if len(sys.argv) > 4 or len(sys.argv) < 3:
+        usage()
+    path = ""
+    algo = ""
+    compare = None
+    # Getting the arguments
+    for i, arg in enumerate(sys.argv):
+        if i == 1:
+            path = arg
+        elif i == 2:
+            algo = arg
+        elif i == 3:
+            if "compare=" in arg:
+                compare = arg.split("=")[1]
+            else:
+                print("Wrong third parameter (maybe forget the compare= ?)", file=sys.stderr)
+                usage()
+    
+    # Do the algorithm and display the result
+    try:
+        with open(path, "rb") as File:
+            bytesFile = File.read()
+            fileHashed = getHash(bytesFile, algo)
+            print("\nHash found:\n")
+            print("-"*len(fileHashed.hexdigest()))
+            print(fileHashed.hexdigest())
+            print("-"*len(fileHashed.hexdigest()))
+            if compare:
+                print("\n<--> Checksum validated: "+ str(fileHashed.hexdigest()==compare)+" <-->\n")
+    except FileNotFoundError:
+        print("\nPath not found", file=sys.stderr)
